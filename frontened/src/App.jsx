f279b96fed2e564d524ff96b1f564eed00b0e4e0
abhaykashyap <abhaykashyap2804@gmail.com>
2025-07-05 23:44:11 +0530

    Initial commit with App.jsx routing and page setup

--- frontened/src/App.jsx
+++ frontened/src/App.jsx
@@ -1,5 +1,55 @@
+// import React from 'react'
+// import {Routes,Route} from 'react-router-dom'
+
+// import Home from './pages/Home'
+// import Collection from './pages/Collection'
+// import Navbar from './components/Navbar'
+// import About from './pages/About'
+// import Cart from './pages/Cart'
+// import Contact from './pages/Contact'
+// import Product from './pages/Product'
+// import PlaceOrder from './pages/PlaceOrder'
+// import Login from './pages/Login'
+// import Footer from './components/Footer'
+// import SearchBar from './components/SearchBar'
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';  
+// import Orders from './pages/Orders';
+// import Verify from './pages/verify'
+
+// const App = () => {
+//   return (
+//     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+//      <ToastContainer/>
+//       <Navbar/>
+//       <SearchBar/> 
+//       <Routes>
+//        <Route path='/' element={<Home/>} /> 
+//        <Route path='/collection' element={<Collection/>} />
+//        <Route path='/about' element={<About/>} />
+//        <Route path='/contact' element={<Contact/>}/>
+//        <Route path='/collection/:subcategory/product/:productId' element={<Product />} />
+//        <Route path='/product/:productId' element={<Product/>}/>
+//        <Route path='/cart' element={<Cart/>}/>
+//        {/* <Route path='/login' element={<Login/>}/> */}
+//        <Route path='/login' element={<Login/>}/>
+//        <Route path='/place-order' element={<PlaceOrder/>}/>
+//        <Route path='/orders' element={<Orders/>} />
+//        <Route path='/verify' element={<Verify/>}/>
+
+//       </Routes>
+//       <Footer/>
+      
+      
+//     </div>
+//   )
+// }
+
+// export default App
+
+
 import React from 'react'
-import {Routes,Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Collection from './pages/Collection'
@@ -12,37 +62,34 @@ import PlaceOrder from './pages/PlaceOrder'
 import Login from './pages/Login'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';  
-import Orders from './pages/Orders';
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Orders from './pages/Orders'
 import Verify from './pages/verify'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-     <ToastContainer/>
-      <Navbar/>
-      <SearchBar/> 
+      <ToastContainer />
+      <Navbar />
+      <SearchBar />
       <Routes>
-       <Route path='/' element={<Home/>} /> 
-       <Route path='/collection' element={<Collection/>} />
-       <Route path='/about' element={<About/>} />
-       <Route path='/contact' element={<Contact/>}/>
-       <Route path='/collection/:subcategory/product/:productId' element={<Product />} />
-       <Route path='/product/:productId' element={<Product/>}/>
-       <Route path='/cart' element={<Cart/>}/>
-       {/* <Route path='/login' element={<Login/>}/> */}
-       <Route path='/login' element={<Login/>}/>
-       <Route path='/place-order' element={<PlaceOrder/>}/>
-       <Route path='/orders' element={<Orders/>} />
-       <Route path='/verify' element={<Verify/>}/>
-
+        <Route path='/' element={<Home />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/collection/:subcategory/product/:productId' element={<Product />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/orders' element={<Orders />} />
+        <Route path='/verify' element={<Verify />} />
       </Routes>
-      <Footer/>
-      
-      
+      <Footer />
     </div>
   )
 }
 
 export default App
+
