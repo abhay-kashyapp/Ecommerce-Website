9a04c3bb15225fa431de925cae8ffc91a71d4513
abhaykashyap <abhaykashyap2804@gmail.com>
2025-07-27 11:59:06 +0530

    Fix: corrected ProductItem import case for Vercel

--- frontened/src/components/ProductItem.jsx
+++ frontened/src/components/ProductItem.jsx
@@ -3,6 +3,7 @@ import { ShopContext } from '../context/ShopContext';
 import { Link } from 'react-router-dom';
 import { assets } from '../assets/assets';
 
+
 const ProductItem = ({id,image,name,price}) => {
 
   const {currency} = useContext(ShopContext);
